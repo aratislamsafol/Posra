cd8fec19d5304b3b37b6bf02135e4ae78fa340a3
aratislamsafol <aratislamsafol@gmail.com>
2025-07-25 01:52:53 +0600

    Basic Environment complete for backend & Frontend

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const router = require('./src/routes/api.js');
+const bodyParser = require('body-parser');
+
+// security
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const jsonwebtoken = require('jsonwebtoken');
+const path = require('path');
+
+// security implementation
+app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
+app.use(hpp());
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, 
+	limit: 100, 
+	standardHeaders: 'draft-8', 
+})
+
+app.use(limiter)
+// BodyParser Implement
+app.use(bodyParser.json());
+
+// connection mongoose
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.js'});
+
+let URI = process.env.MongoURL;
+let OPTION = { user: '', pass: '', autoIndex: true }; 
+// here must use autoIndex for mongoose index unique value
+
+mongoose.connect(URI, OPTION)
+  .then(() => {
+    console.log("MongoDB connection success");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+// Routing Implement
+app.use('/api/v1', router)
+// Undefined Route Implement
+app.use((req, res) => {
+  res.status(404).json({ status: "Failed", data: "Not Found" });
+});
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,2 @@
+RUNNING_PORT = 5000
+MONGOURL = 'mongodb+srv://aratislamsafol:125576@cluster0.g2v7ree.mongodb.net/Posra'
